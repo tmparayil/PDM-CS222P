9cc0c7f04472f2f90409fd91ca6b278c9ce62cd7
Thomas Parayil <thomasparayil@Thomass-MacBook-Pro-2.local>
2020-01-17 01:20:46 -0800

    clean add

--- rbf/pfm.cc
+++ rbf/pfm.cc
@@ -1,4 +1,5 @@
 #include "pfm.h"
+#include <iostream>
 
 PagedFileManager &PagedFileManager::instance() {
     static PagedFileManager _pf_manager = PagedFileManager();
@@ -13,19 +14,67 @@ PagedFileManager::PagedFileManager(const PagedFileManager &) = default;
 
 PagedFileManager &PagedFileManager::operator=(const PagedFileManager &) = default;
 
+bool file_exists(const std::string &fileName)
+{
+    std::ifstream f(fileName.c_str());
+    if(f.good())
+    {
+        f.close();
+        return true;
+    }
+    return false;
+}
+
 RC PagedFileManager::createFile(const std::string &fileName) {
-    return -1;
+
+    if(file_exists(fileName))
+        return -1;
+
+    std::fstream newFile(fileName,std::ios::out);
+    if(!newFile)
+    {
+        std::cout<<"Error in creating the file"<<std::endl;
+        return -1;
+    }
+    newFile.close();
+    return 0;
 }
 
 RC PagedFileManager::destroyFile(const std::string &fileName) {
-    return -1;
+
+    if(!file_exists(fileName))
+        return -1;
+
+    if(std::remove(fileName.c_str()) != 0)
+        return -1;
+
+    return 0;
 }
 
 RC PagedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
+
+    if(fileHandle.getFile() != nullptr)
+    {
+        std::cout<<"Already file stream is open with this handle"<<std::endl;
+        return -1;
+    }
+    if(file_exists(fileName)) {
+        fileHandle.setFile(const_cast<std::string &>(fileName));
+        return 0;
+    }
+
     return -1;
 }
 
 RC PagedFileManager::closeFile(FileHandle &fileHandle) {
+
+    if((fileHandle.getFile()) != nullptr)
+    {
+        fileHandle.closeFile();
+        return 0;
+    }
+
+    std::cout<<"No file stream is open with this handle"<<std::endl;
     return -1;
 }
 
@@ -33,26 +82,92 @@ FileHandle::FileHandle() {
     readPageCounter = 0;
     writePageCounter = 0;
     appendPageCounter = 0;
+    file;
 }
 
 FileHandle::~FileHandle() = default;
 
+void FileHandle::setFile(std::string& fileName) {
+    file = new std::fstream(fileName);
+}
+
+void FileHandle::closeFile()
+{
+    file->close();
+    delete(file);
+}
+
+std::fstream* FileHandle::getFile() {
+    return file;
+}
+
+bool FileHandle::check_file_stream()
+{
+    if(!file->good())
+        return false;
+
+    return true;
+}
+
 RC FileHandle::readPage(PageNum pageNum, void *data) {
-    return -1;
+
+    if(!check_file_stream())
+        return -1;
+
+    if(pageNum >= FileHandle::getNumberOfPages())
+    {
+        return -1;
+    }
+
+    int offset = pageNum * PAGE_SIZE;
+    file->seekg(offset,file->beg);
+    file->read((char*)data, PAGE_SIZE);
+    file->seekg(0,file->beg);
+    readPageCounter++;
+    return 0;
 }
 
 RC FileHandle::writePage(PageNum pageNum, const void *data) {
-    return -1;
+
+    if (!check_file_stream())
+        return -1;
+
+    if (pageNum >= getNumberOfPages())
+        return -1;
+
+    int offset = pageNum * PAGE_SIZE;
+    file->seekp(offset, std::ios_base::beg);
+    file->write((char *)data, PAGE_SIZE);
+    file->seekp(0, std::ios_base::beg);
+    writePageCounter++;
+    return 0;
 }
 
 RC FileHandle::appendPage(const void *data) {
-    return -1;
+
+    if(!check_file_stream())
+        return -1;
+
+    file->seekp(0,std::ios_base::end);
+    file->write((char*)data, PAGE_SIZE);
+    file->seekp(0,std::ios_base::beg);
+    appendPageCounter++;
+    return 0;
 }
 
 unsigned FileHandle::getNumberOfPages() {
-    return -1;
+
+    file->seekg(0,std::ios_base::end);
+    int length = file->tellg();
+    file->seekg(0,std::ios_base::beg);
+    int pages = length / PAGE_SIZE;
+    return pages;
 }
 
 RC FileHandle::collectCounterValues(unsigned &readPageCount, unsigned &writePageCount, unsigned &appendPageCount) {
-    return -1;
+
+    readPageCount = readPageCounter;
+    writePageCount = writePageCounter;
+    appendPageCount = appendPageCounter;
+    return 0;
 }
--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -1,4 +1,11 @@
 #include "rbfm.h"
+#include "pfm.h"
+
+#include <vector>
+#include <cstring>
+#include <climits>
+#include <iostream>
+#include <cmath>
 
 RecordBasedFileManager &RecordBasedFileManager::instance() {
     static RecordBasedFileManager _rbf_manager = RecordBasedFileManager();
@@ -14,29 +21,250 @@ RecordBasedFileManager::RecordBasedFileManager(const RecordBasedFileManager &) =
 RecordBasedFileManager &RecordBasedFileManager::operator=(const RecordBasedFileManager &) = default;
 
 RC RecordBasedFileManager::createFile(const std::string &fileName) {
-    return -1;
+    PagedFileManager& pfm = PagedFileManager::instance();
+    if(pfm.createFile(fileName.c_str()) != 0)
+        return -1;
+    FileHandle fileHandle;
+    pfm.openFile(fileName.c_str(),fileHandle);
+    RecordBasedFileManager::initialise(fileHandle,-1);
+    return 0;
 }
 
 RC RecordBasedFileManager::destroyFile(const std::string &fileName) {
-    return -1;
+    return PagedFileManager::instance().destroyFile(fileName);
 }
 
 RC RecordBasedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance().openFile(fileName,fileHandle);
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance().closeFile(fileHandle);
 }
 
+int RecordBasedFileManager::getRecSize(const void *data, const std::vector<Attribute> &recordDescriptor) {
+
+    bool checknull;
+    uint32_t RecSize = 0;
+    uint32_t nullinfosize = ceil((double) recordDescriptor.size() / 8);
+    u_int32_t bytesforNullIndic = ceil((double) recordDescriptor.size() / CHAR_BIT);
+
+    RecSize += bytesforNullIndic;
+
+    // -- Why are we appending a null field in this method ?
+    unsigned char *null_Fields_Indicator = (unsigned char *) malloc(nullinfosize);
+    memcpy(null_Fields_Indicator, (char *) data, nullinfosize);
+
+    for (int i = 0; i < recordDescriptor.size(); i++) {
+        checknull = null_Fields_Indicator[i / 8] & (1 << (7 - i % 8));
+        if (!checknull) {
+            switch (recordDescriptor[i].type) {
+                case TypeInt:
+                case TypeReal:
+                    RecSize += recordDescriptor[i].length;
+                    break;
+                case TypeVarChar:
+                    int temp = 0;
+                    memcpy(&temp, (char *) data + RecSize, sizeof(int));
+                    RecSize += temp + sizeof(int);
+                    break;
+            }
+        }
+    }
+    free(null_Fields_Indicator);
+    return RecSize;
+}
+
+/**
+ *
+ * This method initialises a new page with the structure.
+ * Returns the free space in the new page
+ * PFM appendPage() is invoked.
+ *
+ * @param fileHandle
+ * @param pageNum
+ * @return int
+ */
+short RecordBasedFileManager::setPage(FileHandle &fileHandle) {
+
+    void* buffer = malloc(PAGE_SIZE);
+    int offset = PAGE_SIZE - 2 * sizeof(int);
+    int recordOffset = 0;
+    int slotOffset = offset;
+
+    memcpy((char*)buffer + offset,(char*)&recordOffset, sizeof(int));
+    memcpy((char*)buffer + offset + sizeof(int),(char*)&slotOffset, sizeof(int));
+    fileHandle.appendPage(buffer);
+
+    free(buffer);
+    return (slotOffset - recordOffset);
+}
+
+/**
+ * Initialise hidden directory for pageNum = -1
+ * -> set HeaderValue
+ * -> update Total pages to 0
+ * Initialise a page in the hidden directory for all other values
+ * -> set free size for that page
+ * -> update total pages
+ * @param fileHandle
+ * @param pageNum
+ */
+void RecordBasedFileManager::initialise(FileHandle &fileHandle,int pageNum) {
+
+    void* buffer = malloc(PAGE_SIZE);
+    if(pageNum != -1)
+    {
+        fileHandle.readPage(0,buffer);
+        int totalPages=0;
+        memcpy((char*)&totalPages,(char*)buffer + HEADER_SIZE, sizeof(int));
+
+        int offset = HEADER_SIZE + sizeof(int) + (pageNum - 1) * sizeof(short);
+        short freeSpace = RecordBasedFileManager::setPage(fileHandle);
+        memcpy((char*)buffer + offset,(char*)&freeSpace, sizeof(short));
+
+        //Refresh the number of pages in hidden page
+        totalPages++;
+        memcpy((char*)buffer + HEADER_SIZE,(char*)&totalPages, sizeof(int));
+        fileHandle.writePage(0,buffer);
+    } else
+    {
+        int temp = HEADER_VAL;
+        int recordNum = 0;
+        // First time creation of hidden page
+        memcpy((char*)buffer,(char*)&temp,HEADER_SIZE);
+        memcpy((char*)buffer + HEADER_SIZE,(char*)&recordNum, sizeof(int));
+        fileHandle.appendPage(buffer);
+    }
+    free(buffer);
+}
+
+void RecordBasedFileManager::updateFreeSpace(int pageNum, short freeSpace, FileHandle &fileHandle) {
+    void* buffer = malloc(PAGE_SIZE);
+    fileHandle.readPage(0,buffer);
+    int offset = HEADER_SIZE + sizeof(int) + (pageNum - 1) * sizeof(short);
+    memcpy((char*)buffer + offset,(char*)&freeSpace, sizeof(short));
+    fileHandle.writePage(0,buffer);
+}
+
+/**
+ * Insert a record into the file
+ *
+ * Get record size. Check hidden directory if free space available in a  page.
+ * If yes -> insert record in that page
+ * If no ->
+ *  -> -> has hidden page been initialised ? If no : ERROR
+ *  -> -> if no pages have free space, append new page and insert record
+ *
+ * @param fileHandle
+ * @param recordDescriptor
+ * @param data
+ * @param rid
+ * @return
+ */
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
                                         const void *data, RID &rid) {
-    return -1;
+    uint32_t recSize = getRecSize(data,recordDescriptor);
+    int header = 0;
+    void* hidden = malloc(PAGE_SIZE);
+
+    fileHandle.readPage(0,hidden);
+    memcpy((char*)&header,(char*)hidden,HEADER_SIZE);
+
+    if(header != HEADER_VAL)
+    {
+//        std::cout<<"Hidden directory not initialized"<<std::endl;
+        return -1;
+    } else
+    {
+        int totalPages=0;
+        bool gotPage = false;
+        //Read number of data pages
+        memcpy((char*)&totalPages,(char*)hidden + HEADER_SIZE, sizeof(int));
+        int page = 0;
+        short freeSize = 0;
+        if(totalPages == 0)
+        {
+            RecordBasedFileManager::initialise(fileHandle,1);
+            totalPages++;
+            page = totalPages;
+        } else
+        {
+            int offset = HEADER_SIZE + sizeof(int);
+            // Looping through all the pages in hidden directory
+            for(int i=1;i<=totalPages;i++)
+            {
+                memcpy((char*)&freeSize,(char*)hidden + offset, sizeof(short));
+                offset += sizeof(short);
+
+                if(freeSize >= recSize + 2* sizeof(int))
+                {
+                    page = i;
+                    gotPage = true;
+                    break;
+                }
+            }
+
+            // If no page in the directory has enough free space for the record
+            if(!gotPage)
+            {
+                RecordBasedFileManager::initialise(fileHandle,totalPages + 1);
+                totalPages++;
+                page = totalPages;
+            }
+        }
+
+        // Reading slot and record offsets
+        void* buffer = malloc(PAGE_SIZE);
+        fileHandle.readPage(page,buffer);
+
+        int ptrOffsets = PAGE_SIZE - 8;
+        int recordOffset , slotOffset;
+
+        memcpy((char*)&recordOffset, (char*)buffer + ptrOffsets, sizeof(int));
+        memcpy((char*)&slotOffset, (char*)buffer + ptrOffsets + sizeof(int), sizeof(int));
+
+        int slotNumber = (ptrOffsets - slotOffset ) / 8;
+        slotNumber++;
+
+        //Updating RID of record
+        rid.pageNum = page;
+        rid.slotNum = slotNumber;
+
+        // Insert new record slot in slot directory
+        memcpy((char*)buffer + slotOffset - 4,(char*)&recSize, sizeof(int));
+        memcpy((char*)buffer + slotOffset - 8,(char*)&recordOffset, sizeof(int));
+        slotOffset -= 2 * sizeof(int);
+
+        // Inserting record
+        memcpy((char*)buffer + recordOffset,(char*)data,recSize);
+        recordOffset += recSize;
+
+        // Updating slot-record pointers after insert
+        memcpy((char*)buffer + ptrOffsets,(char*)&recordOffset, sizeof(int));
+        memcpy((char*)buffer + ptrOffsets + sizeof(int),(char*)&slotOffset, sizeof(int));
+
+        short freeSpace = slotOffset - recordOffset;
+
+        fileHandle.writePage(page,buffer);
+        RecordBasedFileManager::updateFreeSpace(page,freeSpace,fileHandle);
+    }
+    free(hidden);
+    return 0;
 }
 
 RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
                                       const RID &rid, void *data) {
-    return -1;
+    int offset = PAGE_SIZE - 2 * sizeof(int) - rid.slotNum * 2 * sizeof(int);
+    void* buffer = malloc(PAGE_SIZE);
+    int slotOffset , length;
+    fileHandle.readPage(rid.pageNum,buffer);
+    memcpy((char*)&slotOffset,(char*)buffer + offset, sizeof(int));
+    memcpy((char*)&length,(char*)buffer + offset + sizeof(int), sizeof(int));
+
+    memcpy(data,(char*)buffer + slotOffset , length);
+    free(buffer);
+    return 0;
 }
 
 RC RecordBasedFileManager::deleteRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
@@ -45,7 +273,67 @@ RC RecordBasedFileManager::deleteRecord(FileHandle &fileHandle, const std::vecto
 }
 
 RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescriptor, const void *data) {
-    return -1;
+
+    int offset = 0;
+    int nullInfo = ceil((double) recordDescriptor.size() / CHAR_BIT);
+    char* bitInfo = new char[nullInfo];
+
+    memset(bitInfo, 0 , nullInfo);
+    memcpy((char*)bitInfo,(char*)data, nullInfo);
+    offset += nullInfo;
+    for(int itr = 0 ;itr < recordDescriptor.size();itr++) {
+        int bitShift = CHAR_BIT - 1 - itr%CHAR_BIT;
+        int checkNullBit = bitInfo[itr / CHAR_BIT] & (1 << (bitShift));
+        switch(recordDescriptor[itr].type)
+        {
+            case TypeInt:
+                int tempInt;
+                if(!checkNullBit)
+                {
+                    memcpy((char*)&tempInt,(char*)data + offset, sizeof(int));
+                    std::cout<<recordDescriptor[itr].name<<":\t"<<tempInt<<"\t";
+                } else
+                {
+                    std::cout<<recordDescriptor[itr].name<<":\tNULL"<<"\t";
+                }
+
+                offset += sizeof(int);
+                break;
+            case TypeReal:
+                float tempFloat;
+                if(!checkNullBit) {
+                    memcpy((char *) &tempFloat, (char *) data + offset, sizeof(float));
+                    std::cout<<recordDescriptor[itr].name<<":\t"<<tempFloat<<"\t";
+                } else
+                {
+                    std::cout<<recordDescriptor[itr].name<<":\tNULL"<<"\t";
+                }
+                offset += sizeof(float);
+                break;
+            case TypeVarChar:
+                int length;
+                if(!checkNullBit)
+                {
+                    memcpy((char*)&length,(char*)data + offset, sizeof(int));
+                    std::cout<<"Length : "<<length<<std::endl;
+                    offset += sizeof(int);
+
+                    char* tempString = new char[length+1];
+                    memcpy(tempString,(char*)data + offset, length);
+                    tempString[length] = '\0';
+                    std::cout<<recordDescriptor[itr].name<<":\t";
+                    std::cout<<tempString<<"\t";
+                    offset += length;
+
+                } else
+                {
+                    std::cout<<recordDescriptor[itr].name<<":\tNULL"<<"\t";
+                }
+                break;
+        }
+    }
+    std::cout<<std::endl;
+    return 0;
 }
 
 RC RecordBasedFileManager::updateRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
@@ -63,6 +351,3 @@ RC RecordBasedFileManager::scan(FileHandle &fileHandle, const std::vector<Attrib
                                 const std::vector<std::string> &attributeNames, RBFM_ScanIterator &rbfm_ScanIterator) {
     return -1;
 }
-
-
-
